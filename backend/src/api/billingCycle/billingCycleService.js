18bf7d682e38327c0b291748e849c8b1b25385c0
gpfurlaneto <gpfurlaneto@gmail.com>
2017-08-06 23:24:44 -0300

    validação

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,6 +1,6 @@
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions = {new: false, runValidators: true}
+BillingCycle.updateOptions = {new: true, runValidator: true}
 
 module.exports = BillingCycle
