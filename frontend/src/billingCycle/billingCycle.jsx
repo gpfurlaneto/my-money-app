7fa6c708899284f2d4caa110db672e2149d92189
gpfurlaneto <gpfurlaneto@gmail.com>
2017-08-17 00:24:19 -0300

    listagem e cadastro

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,18 +1,60 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabHeader from '../common/tab/tabHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabContent from '../common/tab/tabContent'
+import { selectTab, showTabs } from '../common/tab/tabActions'
+import { create } from './billingCycleActios'
+
+import List from './billingCycleList'
+import Form from './billingCycleForm'
 
 class BillingCycle extends Component {
+
+    componentWillMount() {
+        this.props.selectTab('tabList')
+        this.props.showTabs('tabList', 'tabCreate')
+    }
+
     render() {
         return (
             <div>
-                <ContentHeader title='BillingCycle' small='Versão 1.0'/>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro'/>
                 <Content>
-                    BillingCycle
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id='tabList'>
+                                <List />
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={create}/>
+                            </TabContent>
+                            <TabContent id='tabUpdate'>
+                                <h1>Alterar</h1>
+                            </TabContent>
+                            <TabContent id='tabDelete'>
+                                <h1>Excluir</h1>
+                            </TabContent>
+                        </TabsContent>
+                    </Tabs>  
                 </Content>
             </div>
         )
     }
 }
-export default BillingCycle
+
+const mapDispatchToProps = dispatch => bindActionCreators( 
+    { selectTab, showTabs, create}, dispatch )
+export default connect(null, mapDispatchToProps)(BillingCycle)
