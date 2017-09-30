c64d2e615cda8db76c45a84317b77c2505565c84
gpfurlaneto <gpfurlaneto@gmail.com>
2017-09-29 23:38:22 -0300

    build especificando porta e url de banco

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+
+if(process.env.databaseUrl){
+    console.log(process.env.databaseUrl)
+    module.exports = mongoose.connect(process.env.databaseUrl)
+}else{
+    console.log('base local')
+    module.exports = mongoose.connect('mongodb://localhost/mymoney')
+}
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,5 +1,12 @@
-const port = 3003
+var portArgument = null
 
+if(process.env.port){
+    portArgument = process.env.port
+}else{
+    portArgument = 3003
+}
+
+const port = portArgument
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
